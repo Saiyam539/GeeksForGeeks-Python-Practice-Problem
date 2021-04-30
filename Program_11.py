de5f9cdcb1f3e0a6469cbc91bad9522223a536e8
Saiyam Arora <77787626+Saiyam539@users.noreply.github.com>
2021-04-30 10:12:13 +0530

    Add files via upload

---
+++ Program_11.py
@@ -0,0 +1,13 @@
+# In this program we will make 
+# a empty class in python 
+
+class empty_class:
+    pass
+
+main_class = empty_class()
+print(type(main_class))
+
+'''
+The output of this code will be:- 
+<class '__main__.empty_class'> 
+'''
